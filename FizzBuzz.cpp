38bba48cfa95320d7963fc42cba366f0aa3efd12
John Ellis <johnhell@usc.edu>
2014-04-02 15:11:57 -0700

    Added rule of FizzBuzz

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
  
 int main(int argc, char *argv[]) {
+for(unsigned int i = 0; i < 100; i++) {
+if( i % 5 == 0)
+std::cout << i << ": " << "Buzz" << std::endl;
+}
+return(0);
+}#include <iostream>
+ 
+int main(int argc, char *argv[]) {
   for(unsigned int i = 0; i < 100; i++) {
     std::cout << i << std::endl;
   }
